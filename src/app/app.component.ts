e5ebd84b6c62dcbc31952d7c95c876af5845e4e0
NikolaDonevski138 <n.donevski@yahoo.com>
2018-12-15 20:20:23 +0100

    Adds header component and styles

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+componentTitle = "I am  app component from component.ts";
+clickHandler(){
+alert("I am Clicked");
+ }
+}
+
